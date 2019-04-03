9856b4d4baa27ed451094ab7064ba21449ad24af
Deepak Bhari <deepak.bhari1@Deepaks-MBP.lan>
2019-04-03 19:01:27 +0100

    keep json only necessary for ejs

--- routes/admin.js
+++ routes/admin.js
@@ -13,10 +13,8 @@ router.get('/add-product', (req, res, next) => {
     //res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
     res.render('add-product', {
         pageTitle: 'Add Product',
-        path: '/admin/add-product',
-        formsCSS: true,
-        productCSS: true,
-        activeAddProduct: true
+        path: '/admin/add-product'
+
     });
 });
 
--- routes/shop.js
+++ routes/shop.js
@@ -14,9 +14,7 @@ router.get('/', (req, res, next) => {
         prods: products,
         pageTitle: 'Shop',
         path: '/',
-        hasProducts: products.length > 0,
-        activeShop: true,
-        productCSS: true
+        hasProducts: products.length > 0
     });
 });
 
