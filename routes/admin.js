6f8f406b54c1700de9011cba147e4d9df29f1967
Deepak Bhari <deepak.bhari1@Deepaks-MBP.lan>
2019-04-03 20:47:51 +0100

    take functions out of routers and put into controllers - some clean up

--- routes/admin.js
+++ routes/admin.js
@@ -1,34 +1,14 @@
 const path = require('path');
 const express = require('express');
 
-const rootDir = require('../utils/path');
+const productsController = require('../controllers/products');
 
 const router = express.Router();
 
-const products = [];
-
 //admin/add-product => GET
-router.get('/add-product', (req, res, next) => {
-    //res.sendFile(path.join(__dirname, '..', 'views', 'add-product.html'));
-    //res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
-    res.render('add-product', {
-        pageTitle: 'Add Product',
-        path: '/admin/add-product'
-
-    });
-});
+router.get('/add-product', productsController.getAddProduct);
 
 //admin/add-product => POST
-router.post('/add-product', (req, res, next) => {
-    //body will be undefined unless we use bodyParser middleware first
+router.post('/add-product', productsController.postAddProduct);
 
-    products.push({
-        title: req.body.title
-    });
-
-    res.redirect('/');
-});
-
-//module.exports = router;
-exports.routes = router;
-exports.products = products;
+module.exports = router;
