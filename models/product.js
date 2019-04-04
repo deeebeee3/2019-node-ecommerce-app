90dac2420f915d4911b55da0bfe026a11feaa246
Deepak Bhari <deepak.bhari1@Deepaks-MBP.lan>
2019-04-04 15:08:44 +0100

    refactor and streamline code by using a global path and a helper function

--- models/product.js
+++ models/product.js
@@ -1,52 +1,45 @@
 const fs = require('fs');
 const path = require('path');
 
+const p = path.join(
+    path.dirname(process.mainModule.filename),
+    'data',
+    'products.json'
+);
+
+const getProductsFromFile = (cb) => {
+    //executed
+    //(err, fileContent) => {...} callback is registered in event emitter registry
+    //then finishes with function
+    //fetchAll does not return anything
+    fs.readFile(p, (err, fileContent) => {
+        if (err) {
+            cb([]);
+        } else {
+            cb(JSON.parse(fileContent));
+        }
+    });
+};
+
 module.exports = class Product {
-    constructor(t){
+    constructor(t) {
         this.title = t;
     };
 
     save() {
-        const p = path.join(
-            path.dirname(process.mainModule.filename), 
-            'data', 
-            'products.json'
-        );
-
-        fs.readFile(p, (err, fileContent) => {
-            let products = [];
-            if(!err){
-                products = JSON.parse(fileContent);
-            }
-
+        getProductsFromFile((products) => {
             //if don't use an arrow func, this will lose context and not refer to the class.
             products.push(this);
 
             //now save back to file
-            fs.writeFile(p, JSON.stringify(products), (err) =>{
+            fs.writeFile(p, JSON.stringify(products), (err) => {
                 console.log(err);
             });
         });
     };
 
     static fetchAll(cb) {
-        //executed
-        const p = path.join(
-            path.dirname(process.mainModule.filename), 
-            'data', 
-            'products.json'
-        );
-
-        //executed
-        //(err, fileContent) => {...} callback is registered in event emitter registry
-        //then finishes with function
-        //fetchAll does not return anything
-        fs.readFile(p, (err, fileContent) => {
-            if(err){
-                cb([]);
-            }
-            cb(JSON.parse(fileContent));
-        });
+        getProductsFromFile(cb);
     };
 
 }
