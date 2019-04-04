eadf31196dca5f3e2b7dffda3aa23de098caef3f
Deepak Bhari <deepak.bhari1@Deepaks-MBP.lan>
2019-04-04 14:37:18 +0100

    store products in a file rather than an array

--- models/product.js
+++ models/product.js
@@ -1,4 +1,5 @@
-const products = [];
+const fs = require('fs');
+const path = require('path');
 
 module.exports = class Product {
     constructor(t){
@@ -6,11 +7,41 @@ module.exports = class Product {
     };
 
     save() {
-        products.push(this)
+        const p = path.join(
+            path.dirname(process.mainModule.filename), 
+            'data', 
+            'products.json'
+        );
+
+        fs.readFile(p, (err, fileContent) => {
+            let products = [];
+            if(!err){
+                products = JSON.parse(fileContent);
+            }
+
+            //if don't use an arrow func, this will lose context and not refer to the class.
+            products.push(this);
+
+            //now save back to file
+            fs.writeFile(p, JSON.stringify(products), (err) =>{
+                console.log(err);
+            });
+        });
     };
 
     static fetchAll() {
-        return products;
+        const p = path.join(
+            path.dirname(process.mainModule.filename), 
+            'data', 
+            'products.json'
+        );
+
+        fs.readFile(p, (err, fileContent) => {
+            if(err){
+                return []
+            }
+            return JSON.parse(fileContent);
+        });
     };
 
 }
