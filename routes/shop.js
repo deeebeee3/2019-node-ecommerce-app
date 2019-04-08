4e0a9458e41d8a44fb88c5e89041584ffb282bb1
Deepak Bhari <deepak.bhari1@Deepaks-MBP.lan>
2019-04-08 19:11:03 +0100

    extracting dynamic params

--- routes/shop.js
+++ routes/shop.js
@@ -9,6 +9,11 @@ router.get('/', shopController.getIndex);
 
 router.get('/products', shopController.getProducts);
 
+//more specific routes need to be above routes with a dynamic segment :...
+//router.get('/products/delete', doSomething);
+
+router.get('/products/:productId', shopController.getProduct)
+
 router.get('/cart', shopController.getCart);
 
 router.get('/orders', shopController.getOrders);
