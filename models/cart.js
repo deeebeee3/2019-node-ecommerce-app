b1bb6abe50da35a032a96ebfdfe25b0dfcbb04c2
Deepak Bhari <deepak.bhari1@Deepaks-MBP.lan>
2019-04-17 01:13:52 +0100

    fix delete product bug - occured when trying to delete a product that was not in the cart

--- models/cart.js
+++ models/cart.js
@@ -48,6 +48,11 @@ module.exports = class Cart {
             }
             const updatedCart = { ...JSON.parse(fileContent) };
             const product = updatedCart.products.find(prod => prod.id === id);
+            
+            if(!product){
+                return;
+            }
+
             const productQty = product.qty;
 
             //all products except one i want to delete
