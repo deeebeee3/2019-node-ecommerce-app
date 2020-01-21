5a819a1520d14df02c4c4e0d196b4a2ed736f521
Deepak Bhari <deepakbhari@Deepaks-MacBook-Pro.local>
2020-01-21 15:52:16 +0000

    fetching products from db

--- controllers/shop.js
+++ controllers/shop.js
@@ -2,22 +2,23 @@ const Product = require('../models/product');
 const Cart = require('../models/cart');
 
 exports.getProducts = (req, res, next) => {
-    //pass function to be executed - (products) => {...} - once products read from file
-    Product.fetchAll((products) => {
-        res.render('shop/product-list', {
-            prods: products,
-            pageTitle: 'All Products',
-            path: '/products'
-        });
-    });
+    Product.fetchAll()
+        .then(([rows, fieldData]) => {
+            res.render('shop/product-list', {
+                prods: rows,
+                pageTitle: 'All Products',
+                path: '/products'
+            });
+        })
+        .catch(err => console.log(err));
 };
 
-exports.getProduct = (req, res, next) =>{
+exports.getProduct = (req, res, next) => {
     const prodId = req.params.productId;
-    
+
     Product.findById(prodId, product => {
         res.render('shop/product-detail', {
-            product: product, 
+            product: product,
             pageTitle: product.title,
             path: '/products'
         });
@@ -25,13 +26,15 @@ exports.getProduct = (req, res, next) =>{
 }
 
 exports.getIndex = (req, res, next) => {
-    Product.fetchAll((products) => {
-        res.render('shop/index', {
-            prods: products,
-            pageTitle: 'Shop',
-            path: '/'
-        });
-    });
+    Product.fetchAll()
+        .then(([rows, fieldData]) => {
+            res.render('shop/index', {
+                prods: rows,
+                pageTitle: 'Shop',
+                path: '/'
+            });
+        })
+        .catch(err => console.log(err));
 }
 
 exports.getCart = (req, res, next) => {
@@ -39,9 +42,9 @@ exports.getCart = (req, res, next) => {
         Product.fetchAll(products => {
             const cartProducts = [];
             //got cart and got products now just need to filter out products actually in the cart
-            for(product of products){
+            for (product of products) {
                 const cartProductData = cart.products.find(prod => prod.id === product.id);
-                if(cartProductData){
+                if (cartProductData) {
                     cartProducts.push({ productData: product, qty: cartProductData.qty });
                 }
             }
--- models/product.js
+++ models/product.js
@@ -1,27 +1,5 @@
-const fs = require('fs');
-const path = require('path');
-
+const db = require('../utils/database');
 const Cart = require('./cart');
-
-const p = path.join(
-    path.dirname(process.mainModule.filename),
-    'data',
-    'products.json'
-);
-
-const getProductsFromFile = (cb) => {
-    //executed
-    //(err, fileContent) => {...} callback is registered in event emitter registry
-    //then finishes with function
-    //fetchAll does not return anything
-    fs.readFile(p, (err, fileContent) => {
-        if (err) {
-            cb([]);
-        } else {
-            cb(JSON.parse(fileContent));
-        }
-    });
-};
 
 module.exports = class Product {
     constructor(id, title, imageUrl, price, description) {
@@ -33,67 +11,18 @@ module.exports = class Product {
     };
 
     save() {
-        getProductsFromFile((products) => {
-            if(this.id){
-                //Replace an existing product in array with updated product
-                const existingProductIndex = products.findIndex(prod => prod.id === this.id);
-                const updatedProducts = [...products];
-                updatedProducts[existingProductIndex] = this;
-
-                fs.writeFile(p, JSON.stringify(updatedProducts), (err) => {
-                    if(err){
-                        console.log(err);
-                    }
-                });
-            }else{
-                //Create a new product
-                //adds a new id property to the entire product model we're working on
-                this.id = Math.round((Math.random() * 10000000000)).toString();
-
-                //if don't use an arrow func, this will lose context and not refer to the class.
-                products.push(this);
-    
-                //now save back to file
-                fs.writeFile(p, JSON.stringify(products), (err) => {
-                    if(err){
-                        console.log(err);
-                    }
-                });
-            }
-        });
     };
 
-    static deleteById(id){
-        getProductsFromFile(products => {
-            const product = products.find(prod => prod.id === id);
-            
-            //creates an array filled with all array elements that pass a test
-            //keep all products i dont want to delete
-            const updatedProducts = products.filter(prod => prod.id !== id); //filter is synchronous
-
-            //save all products except for the one i want to delete back to the file
-            fs.writeFile(p, JSON.stringify(updatedProducts), err => {
-                if(!err){
-                    // now delete product from cart...
-                    Cart.deleteProduct(id, product.price);
-                }
-            });
-        });
+    static deleteById(id) {
     }
 
-    static fetchAll(cb) {
-        getProductsFromFile(cb);
+    static fetchAll() {
+        return db.execute('SELECT * FROM products');
+
     };
 
-    static findById(id, cb){
-        getProductsFromFile(products => {
-            // const product = products.find(p => {
-            //     return p.id === id;
-            // });
-
-            const product = products.find(p => p.id === id); //find is synchronous
-            cb(product);
-        });
+    static findById(id) {
     }
-
 }
+
+
