39c743f90c130226280a0b760382f3f2b90f0260
Deepak Bhari <deepakbhari@deepaks-mbp.lan>
2020-02-05 00:59:35 +0000

    relationships - adding a one to many relation

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const bodyParser = require('body-parser');
 const errorController = require('./controllers/error');
 const sequelize = require('./util/database');
 
+const Product = require('./models/product');
+const User = require('./models/user');
+
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -30,7 +33,11 @@ app.use(shopRoutes);
 //catch all middleware - use - handle all http methods
 app.use(errorController.get404);
 
-sequelize.sync().then(result => {
+Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
+User.hasMany(Product);
+
+//dont use force: true in production - dont want to overwrite tables...
+sequelize.sync({ force: true }).then(result => {
     // console.log(result);
 }).catch(err => {
     console.log(err);
