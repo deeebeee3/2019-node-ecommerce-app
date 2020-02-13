6f21a3edeb00e4efe2408030f7de3f23ee98e40c
Deepak Bhari <deepakbhari@deepaks-mbp.lan>
2020-02-13 03:29:43 +0000

    Resetting the Cart and Fetching and Outputting Orders

--- controllers/shop.js
+++ controllers/shop.js
@@ -145,15 +145,17 @@ exports.postOrder = (req, res, next) => {
 }
 
 exports.getOrders = (req, res, next) => {
-    res.render('shop/orders', {
-        pageTitle: 'Orders',
-        path: '/orders'
-    });
-}
+    req.user
+        .getOrders({ include: ['products'] })
+        .then(orders => {
+            res.render('shop/orders', {
+                pageTitle: 'Orders',
+                path: '/orders',
+                orders: orders
+            });
+        })
+        .catch(err => console.log(err));
 
-exports.getCheckout = (req, res, next) => {
-    res.render('shop/checkout', {
-        pageTitle: 'Checkout',
-        path: '/checkout'
-    });
+
+
 }
